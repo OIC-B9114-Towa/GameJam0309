826c6f04adf01024f2cb36d02c2023ef15499c05
hiroki <hiroking.na0622@gmail.com>
2022-03-11 06:48:43 +0900

    タイトル画面作成　メニューアイコンの追加

--- 生け花ダーツ/Project/SceneTitle.cpp
+++ 生け花ダーツ/Project/SceneTitle.cpp
@@ -1,7 +1,19 @@
 #include "SceneTitle.h"
 
 CSceneTitle::CSceneTitle() :
-m_fileBuffer(NULL)
+	m_titleLogo(),
+	m_titleBack(),
+	m_menuIcon(),
+	m_menuFont(),
+	m_menuBackColor{0,0,0},
+	m_menuFrameColor{0,0,0},
+	m_menuRect(0,0,0,0),
+	m_selectMenu(0),
+	m_logoAlpha(0),
+	m_menuAlpha{0,0,0},
+	m_windowAlpha(0),
+	m_fileBuffer(NULL),
+	m_menuDesc{NULL,NULL,NULL}
 {
 
 }
@@ -40,24 +52,108 @@ void CSceneTitle::LoadText()
 
 }
 
-
 void CSceneTitle::Initialize() {
+	//テクスチャ読み込み
 	m_titleLogo.Load("Texture\\TitleLogo.png");
 	m_titleBack.Load("Texture\\TitleBack.png");
+
+	m_menuIcon[SELECTMENU_TOGAME].Load("Texture\\MainGameIcon.png");
+	m_menuIcon[SELECTMENU_TOCOLLECTION].Load("Texture\\CollectionIcon.png");
+	m_menuIcon[SELECTMENU_TOOPTION].Load("Texture\\OptionIcon.png");
+	m_menuIcon[SELECTMENU_QUIT].Load("Texture\\QuitIcon.png");
+
+	//最初は[メインゲーム]に合わせる
 	m_selectMenu = SELECTMENU_TOGAME;
+
+	//アルファ値の初期化
 	m_logoAlpha = 0;
 	m_windowAlpha = 0;
 	for (int i = 0; i < SELECTMENU_COUNT; i++)
 	{
 		m_menuAlpha[i] = 0;
+
+		//説明文格納用・初期化
+		m_menuDesc[i] = NULL;
 	}
+
 	m_fileBuffer = NULL;
+	//テキストから読み込み
 	LoadText();
-	m_menuFont.Create(40, "MS 明朝");
+	//フォント設定
+	m_menuFont.Create(m_fontSize, "MS 明朝");
+	
+	//メニューの座標設定
+	m_menuRect = CRectangle(m_initMenuRect);
+	
+	//メニューの色設定
+	m_menuBackColor = { m_offMenuBackColor.red,m_offMenuBackColor.green, m_offMenuBackColor.blue };
+	m_menuFrameColor = { m_offMenuFrameColor.red,m_offMenuFrameColor.green, m_offMenuFrameColor.blue };
 }
 
-void CSceneTitle::Update() {
+//透明度の更新
+void CSceneTitle::UpdateAlpha()
+{
+	if (m_logoAlpha < FF)
+	{
+		m_logoAlpha += m_logoAlphaSpeed;
+		if (m_logoAlpha >= FF - 1)
+			m_logoAlpha = FF;
+	}
+	if (m_windowAlpha < WINDOW_ALPHA_STOP_TIMING)
+	{
+		if (m_logoAlpha > WINDOW_ALPHA_START_TIMING)
+			m_windowAlpha += m_windowAlphaSpeed;
+	}
+	for (int i = 0; i < SELECTMENU_COUNT; i++)
+	{
+		if (m_menuAlpha[i] < FF)
+		{
+			if ((i == SELECTMENU_TOGAME && m_logoAlpha >= WINDOW_ALPHA_START_TIMING) ||
+				(i == SELECTMENU_TOCOLLECTION && m_menuAlpha[SELECTMENU_TOGAME] >= MENU_ALPHA_START_TIMING) ||
+				(i == SELECTMENU_TOOPTION && m_menuAlpha[SELECTMENU_TOCOLLECTION] >= MENU_ALPHA_START_TIMING) ||
+				(i == SELECTMENU_QUIT && m_menuAlpha[SELECTMENU_TOOPTION] >= MENU_ALPHA_START_TIMING))
+			{
+				m_menuAlpha[i] += m_menuAlphaSpeed;
+			}
+		}
+	}
+}
 
+//メニュー選択の更新
+void CSceneTitle::UpdateMenu()
+{
+	//矢印で項目の移動
+	if (g_pInput->IsKeyPush(MOFKEY_DOWNARROW) || g_pInput->IsKeyPush(MOFKEY_S))
+	{
+		//効果音：カーソル移動音
+
+		if (m_selectMenu == SELECTMENU_COUNT - 1)
+		{
+			m_selectMenu = SELECTMENU_TOGAME;
+		}
+		else
+		{
+			m_selectMenu++;
+		}
+	}
+	if (g_pInput->IsKeyPush(MOFKEY_UPARROW) || g_pInput->IsKeyPush(MOFKEY_W))
+	{
+		//効果音：カーソル移動音
+
+		if (m_selectMenu == SELECTMENU_TOGAME)
+		{
+			m_selectMenu = SELECTMENU_COUNT - 1;
+		}
+		else
+		{
+			m_selectMenu--;
+		}
+	}
+}
+
+//メニュー選んだとき、シーンの遷移更新
+void CSceneTitle::UpdateTransition()
+{
 	//とりあえず [Space] で画面遷移
 	if (g_pInput->IsKeyPush(MOFKEY_SPACE))
 	{
@@ -87,99 +183,104 @@ void CSceneTitle::Update() {
 		//現在のシーンを閉じる
 		m_bEnd = true;
 	}
+}
 
-	//矢印で項目の移動
-	if (g_pInput->IsKeyPush(MOFKEY_DOWNARROW) || g_pInput->IsKeyPush(MOFKEY_S))
+void CSceneTitle::Update() {
+
+	//透明度の更新
+	UpdateAlpha();
+
+	//メニューのアルファ処理が終わるまで操作不可
+	if (m_menuAlpha[SELECTMENU_COUNT - 1] < FF)
 	{
-		//効果音：カーソル移動音
-
-		if (m_selectMenu == SELECTMENU_COUNT - 1)
-		{
-			m_selectMenu = SELECTMENU_TOGAME;
-		}
-		else
-		{
-			m_selectMenu++;
-		}
-	}
-	if (g_pInput->IsKeyPush(MOFKEY_UPARROW) || g_pInput->IsKeyPush(MOFKEY_W))
-	{
-		//効果音：カーソル移動音
-
-		if (m_selectMenu == SELECTMENU_TOGAME)
-		{
-			m_selectMenu = SELECTMENU_COUNT - 1;
-		}
-		else
-		{
-			m_selectMenu--;
-		}
+		return;
 	}
 
-	if (m_logoAlpha < FF)
-	{
-		m_logoAlpha += 2;
-		if (m_logoAlpha >= FF - 1)
-			m_logoAlpha = FF;
-	}
-	if (m_windowAlpha < FF / 2)
-	{
-		if(m_logoAlpha > 100)
-		m_windowAlpha++;
-	}
-	for (int i = 0; i < SELECTMENU_COUNT; i++)
-	{
-		if (m_menuAlpha[i] < FF)
-		{
-			if ((i == SELECTMENU_TOGAME && m_logoAlpha >= 100) ||
-				(i == SELECTMENU_TOCOLLECTION && m_menuAlpha[SELECTMENU_TOGAME] >= 60) ||
-				(i == SELECTMENU_TOOPTION && m_menuAlpha[SELECTMENU_TOCOLLECTION] >= 60) ||
-				(i == SELECTMENU_QUIT && m_menuAlpha[SELECTMENU_TOOPTION] >= 60))
-			{
-				m_menuAlpha[i] += MENU_ALPHASPEED;
-			}
-		}
-	}
-
+	//メニュー選んだとき、シーンの遷移更新
+	UpdateTransition();
+	//選択項目の更新
+	UpdateMenu();
 }
 
 void CSceneTitle::UpdateDebug() {
-
 }
 
-void CSceneTitle::Render() {
-	//m_titleBack.Render(0, 0);
-
+//選択項目の描画
+void CSceneTitle::RenderMenu()
+{
 	//選択項目
 	for (int i = 0; i < SELECTMENU_COUNT; i++)
-	{
+	{	
 		if (m_selectMenu == i && m_menuAlpha[SELECTMENU_COUNT - 1] >= FF)
 		{
-			CGraphicsUtilities::RenderFillRect(190, 400 + (i * 60), 410, 440 + (i * 60), MOF_XRGB(255, 200, 100));
-			CGraphicsUtilities::RenderRect(190, 400 + (i * 60), 410, 440 + (i * 60), MOF_XRGB(0, 0, 0));
-
-			m_menuFont.RenderString(190, 400 + (i * 60), MOF_COLOR_BLACK, m_menuName[i]);
+			m_menuRect.Left = m_initMenuRect.Left - ON_CURSOR_SHIFT;
+			m_menuRect.Right = m_initMenuRect.Right - ON_CURSOR_SHIFT;
+			m_menuBackColor = { m_onMenuBackColor.red,m_onMenuBackColor.green, m_onMenuBackColor.blue };
+			m_menuFrameColor = { m_onMenuFrameColor.red,m_onMenuFrameColor.green, m_onMenuFrameColor.blue };
 		}
 		else
 		{
-			CGraphicsUtilities::RenderFillRect(200, 400 + (i * 60), 420, 440 + (i * 60), MOF_ARGB(m_menuAlpha[i], 255, 255, 150));
-			CGraphicsUtilities::RenderRect(200, 400 + (i * 60), 420, 440 + (i * 60), MOF_ARGB(m_menuAlpha[i], 100, 100, 100));
+			m_menuRect.Left = m_initMenuRect.Left;
+			m_menuRect.Right = m_initMenuRect.Right;
+			m_menuBackColor = { m_offMenuBackColor.red,m_offMenuBackColor.green, m_offMenuBackColor.blue };
+			m_menuFrameColor = { m_offMenuFrameColor.red,m_offMenuFrameColor.green, m_offMenuFrameColor.blue };
+		}
 
-			m_menuFont.RenderString(200, 400 + (i * 60), MOF_ARGB(m_menuAlpha[i], 0, 0, 0), m_menuName[i]);
+		CGraphicsUtilities::RenderFillRect(m_menuRect.Left, m_menuRect.Top + (i * MENU_INTERVAL), m_menuRect.Right, m_menuRect.Bottom + (i * MENU_INTERVAL),
+			MOF_ARGB(m_menuAlpha[i], m_menuBackColor.red, m_menuBackColor.green, m_menuBackColor.blue));
+		CGraphicsUtilities::RenderRect(m_menuRect.Left, m_menuRect.Top + (i * MENU_INTERVAL), m_menuRect.Right, m_menuRect.Bottom + (i * MENU_INTERVAL),
+			MOF_ARGB(m_menuAlpha[i], m_menuFrameColor.red, m_menuFrameColor.green, m_menuFrameColor.blue));
+
+		m_menuFont.RenderString(m_menuRect.Left + ADJUST_MENU_TEXT, m_menuRect.Top + (i * MENU_INTERVAL) + ADJUST_MENU_TEXT,
+			MOF_ARGB(m_menuAlpha[i], m_menuTextColor.red, m_menuTextColor.green, m_menuTextColor.blue), m_menuName[i]);
+	}
+}
+
+//アイコンの描画
+void CSceneTitle::RenderIcon()
+{
+	CGraphicsUtilities::RenderFillRect(m_iconWindowRect, MOF_ARGB(m_windowAlpha, m_iconWindowBackColor.red, m_iconWindowBackColor.green, m_iconWindowBackColor.blue));
+	CGraphicsUtilities::RenderRect(m_iconWindowRect, MOF_ARGB(m_windowAlpha, m_iconWindowFrameColor.red, m_iconWindowFrameColor.green, m_iconWindowFrameColor.blue));
+	for (int i = 0; i < SELECTMENU_COUNT; i++)
+	{
+		if (m_menuAlpha[SELECTMENU_COUNT - 1] >= FF)
+		{
+			if (i == m_selectMenu)
+				m_menuIcon[i].Render(m_iconPos.x, m_iconPos.y);
 		}
 	}
+}
 
-	//ロゴ
-	m_titleLogo.Render(100, 100,MOF_ARGB(m_logoAlpha, FF, FF, FF));
-
-	//説明の枠
-	CGraphicsUtilities::RenderFillRect(500, 600, 900, 700,MOF_ARGB(m_windowAlpha,255,255,200));
-	CGraphicsUtilities::RenderRect(500, 600, 900, 700, MOF_ARGB(m_windowAlpha, 0, 100, 255));
+//説明
+void CSceneTitle::RenderMenuDescription()
+{
+	CGraphicsUtilities::RenderFillRect(m_descWindowRect, MOF_ARGB(m_windowAlpha, m_descWindowBackColor.red, m_descWindowBackColor.green, m_descWindowBackColor.blue));
+	CGraphicsUtilities::RenderRect(m_descWindowRect, MOF_ARGB(m_windowAlpha, m_descWindowFrameColor.red, m_descWindowFrameColor.green, m_descWindowFrameColor.blue));
 	if (m_menuAlpha[SELECTMENU_COUNT - 1] >= FF)
 	{
-		CGraphicsUtilities::RenderString(510, 610, MOF_COLOR_BLACK, m_menuDesc[m_selectMenu]);
+		CGraphicsUtilities::RenderString(m_descPos.x, m_descPos.y, MOF_COLOR_BLACK, m_menuDesc[m_selectMenu]);
 	}
+}
 
+void CSceneTitle::Render() {
+
+	//背景
+	m_titleBack.Render(0, 0);
+
+	//選択項目
+	RenderMenu();
+
+	//ロゴ
+	m_titleLogo.Render(m_logoPos.x,m_logoPos.y,MOF_ARGB(m_logoAlpha, FF, FF, FF));
+
+	//アイコン
+	RenderIcon();
+	
+	//説明
+	RenderMenuDescription();
+	
+	//操作方法
+	CGraphicsUtilities::RenderString(m_howToTextPos.x, m_howToTextPos.y, MOF_COLOR_BLACK, "[↑↓]で選択　[Space]で決定");
 }
 
 void CSceneTitle::RenderDebug() {
@@ -194,6 +295,10 @@ void CSceneTitle::Release() {
 	//テクスチャの解放
 	m_titleLogo.Release();
 	m_titleBack.Release();
+	for (int i = 0; i < SELECTMENU_COUNT; i++)
+	{
+		m_menuIcon[i].Release();
+	}
 	//フォントの解放
 	m_menuFont.Release();
 	//確保したファイルバッファを解放
--- 生け花ダーツ/Project/SceneTitle.h
+++ 生け花ダーツ/Project/SceneTitle.h
@@ -3,8 +3,17 @@
 
 //色とかの設定最大値
 #define FF 255
-//項目の見えるようになる速度
-#define MENU_ALPHASPEED 3
+//カーソルを合わせているとき左に飛び出す長さ
+#define ON_CURSOR_SHIFT 10
+//メニュー内の文字位置調整
+#define ADJUST_MENU_TEXT 5
+//メニューの間隔
+#define MENU_INTERVAL 60
+//透明度更新タイミング
+#define WINDOW_ALPHA_START_TIMING 100
+#define MENU_ALPHA_START_TIMING 60
+//枠の透明度止めるタイミング
+#define WINDOW_ALPHA_STOP_TIMING 200
 
 //選べる項目の列挙
 enum SelectMenu
@@ -17,35 +26,98 @@ enum SelectMenu
 	SELECTMENU_COUNT,
 };
 
+//3色定義用
+struct RGB_Color
+{
+	int red;
+	int green;
+	int blue;
+};
+
 class CSceneTitle : public CSceneBase
 {
 private:
 	CTexture m_titleLogo;
 	CTexture m_titleBack;
+	CTexture m_menuIcon[SELECTMENU_COUNT];
 
 	CFont m_menuFont;
+	//フォントサイズ
+	const int m_fontSize = 40;
+
+	//色
+	//説明枠
+	const RGB_Color m_descWindowBackColor = { 200,200,255 };
+	const RGB_Color m_descWindowFrameColor = { 0,100,255 };
+	//アイコン枠
+	const RGB_Color m_iconWindowBackColor = { 200,200,210 };
+	const RGB_Color m_iconWindowFrameColor = { 0,100,255 };
+	//選ばれている状態のメニュー
+	const RGB_Color m_onMenuBackColor = { 255,180,100 };
+	const RGB_Color m_onMenuFrameColor = { 0,0,0 };
+	//選ばれていない状態のメニュー
+	const RGB_Color m_offMenuBackColor = { 255,255,150 };
+	const RGB_Color m_offMenuFrameColor = { 100,100,100 };
+	//メニュー
+	RGB_Color m_menuBackColor;
+	RGB_Color m_menuFrameColor;
+	//メニュー内文字
+	const RGB_Color m_menuTextColor = { 0, 0, 0 };
+
+	//描画位置
+	//説明枠
+	const CRectangle m_descWindowRect = CRectangle(500, 470, 800, 540);
+	//アイコン枠
+	const CRectangle m_iconWindowRect = CRectangle(490, 320, 810, 550);
+	//メニュー
+	CRectangle m_menuRect;
+	const CRectangle m_initMenuRect = CRectangle(210, 320, 440, 370);
+	//ロゴ
+	const CVector2 m_logoPos = CVector2(110, 70);
+	//アイコン
+	const CVector2 m_iconPos = CVector2(525, 330);
+	//説明
+	const CVector2 m_descPos = CVector2(510, 480);
+	//操作方法
+	const CVector2 m_howToTextPos = CVector2(320, 580);
 
 	//どの項目を選択中か
 	int m_selectMenu;
 
+	//透明度
+	//ロゴ
 	int	m_logoAlpha;
+	const int m_logoAlphaSpeed = 2;
+	//メニュー
 	int m_menuAlpha[SELECTMENU_COUNT];
+	const int m_menuAlphaSpeed = 3;
+	//枠
 	int m_windowAlpha;
+	const int m_windowAlphaSpeed = 2;
 
+	//テキスト読み込み用
 	char* m_fileBuffer;
 	char* m_menuDesc[SELECTMENU_COUNT];
-	char* m_menuName[SELECTMENU_COUNT] = { 
+
+	//メニュー内文字
+	const char* m_menuName[SELECTMENU_COUNT] = { 
 		"メインゲーム",
-		"コレクション",
-		" オプション ",
-		"  お わ る  " };
+		" コレクション",
+		"  オプション ",
+		"    おわる   " };
 
 public:
 	CSceneTitle();
 	void LoadText();
 	void Initialize(void);
+	void UpdateAlpha();
+	void UpdateMenu();
+	void UpdateTransition();
 	void Update(void);
 	void UpdateDebug(void);
+	void RenderMenu();
+	void RenderIcon();
+	void RenderMenuDescription();
 	void Render(void);
 	void RenderDebug(void);
 	void Release(void);
